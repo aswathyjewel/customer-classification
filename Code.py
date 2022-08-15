8a6fac5bfa2e320dd5ba7843faa645143e27a8c4
aswathyjewel <111308614+aswathyjewel@users.noreply.github.com>
2022-08-15 14:59:19 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -5,10 +5,7 @@
 
 
 #---------------------------------------------------------------
-# @author Bhargav Vemula C0818081
-# @author Govind Vijayan C0819805
-# @author Jaison Kayamkattil Jacob C0814631
-# @author Prasanth Moothedath Padmakumar C0796752
+# @author Aswathy Jewel
 
 # Dataset File downloaded from https://archive.ics.uci.edu/ml/datasets/online+retail
 
